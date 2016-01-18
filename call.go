0a75e2299cc2aaa3af2c30a08833105b24dcc064
Adrien Delorme <gratator@gmail.com>
2016-01-18 12:11:35 +0100

    doc(header): put more words in there :)

--- call.go
+++ call.go
@@ -1,4 +1,53 @@
 // Small pkg that helps you retry things that can be.
+//
+//  Given a func Fn that might fail and return an error.
+//   if the error `IsRetryAble`, Fn will be rerun.
+//   until MaxRetry is reached.
+//
+// If you had a foo lib for a foo service you could:
+//   package foo
+//
+//   import (
+//       "github.com/azr/backoff"
+//       "github.com/azr/retry"
+//   )
+//
+//   func Retry(fn func() error) retry.Call {
+//     return retry.Call{
+//         Fn:            fn,
+//         OnRetry:       GetBackoff().BackOff,
+//         IsRetryableFn: IsErrorRetryable,
+//         MaxRetry:      2,
+//     }
+//   }
+//
+//   func GetBackoff() backoff.Interface {
+//      return backoff.NewExponential()
+//       configure backoffer
+//   }
+//
+//   func IsErrorRetryable(err error) bool {
+//       if err == SomeRetryableError {
+//           return true
+//       }
+//       return false
+//   }
+//
+//   func SomeFunc() error {
+//       err := log.Output(1, "foo")
+//       return err
+//   }
+//
+// Now you can just :
+//
+//   package main
+//
+//   import "bar/foo"
+//
+//   func main() {
+//       err := foo.Retry(foo.SomeFunc).Run()
+//   }
+//
 package retry
 
 type Call struct {
